de8a6ce498ba843d4ba2849e70c9c482e08cfd31
ben black <weepingwillowben@gmail.com>
2018-06-03 18:19:18 -0700

    added mouse movement code

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ GLFWwindow* window;
 // Include GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include "glm/gtx/string_cast.hpp"
 using namespace glm;
 
 #include <stdio.h>
@@ -29,7 +30,8 @@ using namespace std;
 #include <chrono>
 #include "cube_data.h"
 
-
+constexpr int X_WIN_SIZE = 1024;
+constexpr int Y_WIN_SIZE = 768;
 
 GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
 void setup_window();
@@ -67,13 +69,120 @@ void add_cube(CubeData cube, std::vector<float> & color_buffer, std::vector<floa
     add_cube_colors(cube.color,color_buffer);
     add_cube_vertex(cube.coord,vertex_buffer);
 }*/
+glm::mat4 rotate_xy(float theta){
+    return glm::mat4{
+                glm::vec4(cos(theta),-sin(theta),0,0),
+                glm::vec4(sin(theta),cos(theta),0,0),
+                glm::vec4(0,0,1,0),
+                glm::vec4(0,0,0,1),
+            };
+}
+glm::mat4 rotate_xz(float theta){
+    return glm::mat4{
+                glm::vec4(cos(theta),0,-sin(theta),0),
+                glm::vec4(0,1,0,0),
+                glm::vec4(sin(theta),0,cos(theta),0),
+                glm::vec4(0,0,0,1),
+            };
+}
+glm::mat4 get_roated_mat(glm::vec3 pos, float theta1, float theta2){
+    glm::mat4 identity;
+    glm::mat4 translate = glm::mat4{
+                glm::vec4(1,0,0,pos.x),
+                glm::vec4(0,1,0,pos.y),
+                glm::vec4(0,0,1,pos.z),
+                glm::vec4(0,0,0,1),
+    };
+    glm::mat4 rotate_t1 = rotate_xy(theta1);
+    glm::mat4 rotate_t2 = rotate_xz(theta2);
+    return rotate_t2 * rotate_t1 * translate * identity;
+}
+struct CameraPosition{
+    glm::vec3 pos;
+    glm::vec3 zdir;
+    CameraPosition(glm::vec3 init_pos, glm::vec3 init_dir):
+        pos(init_pos),
+        zdir(glm::normalize(init_dir)){}
+    glm::mat4 veiw_mat(){
+        zdir = glm::normalize(zdir);
+        glm::mat4 View       = glm::lookAt(
+                                    pos, // Camera is at (4,3,-3), in World Space
+                                    pos+zdir, // and looks at the origin
+                                    glm::normalize(glm::vec3(0,1,0))   // Head is up (set to 0,-1,0 to look upside-down)
+                               );
+        cout << glm::to_string(View) << endl;
+        return View;
+    }
+    void update_dir(float xoffset, float yoffset){
+        mat4 model_to_camera = veiw_mat();
+        mat4 camera_to_model = glm::inverse(model_to_camera);
+        vec4 new_camera_vec = (model_to_camera * vec4(zdir,0)) + vec4(xoffset,yoffset,0,0);
+        vec4 new_z_dir = camera_to_model*new_camera_vec;
+        vec3 new_dir(new_z_dir.x,new_z_dir.y,new_z_dir.z);
+        zdir = glm::normalize(new_dir);
+    }
+    void move_pos_in_dir(float amt){
+        pos += zdir * amt;
+    }
+    void move_right(float amt){
+        mat4 model_to_camera = veiw_mat();
+        mat4 camera_to_model = glm::inverse(model_to_camera);
+        vec4 add_vec = (camera_to_model*vec4(1,0,0,0)) * amt;
+        pos += vec3(add_vec.x,add_vec.y,add_vec.z);
+    }
+};
+void move_cursor(CameraPosition & camera_pos){
+    static double lastTime = glfwGetTime();
+    double currentTime = glfwGetTime();
+    double time_delta = currentTime - lastTime;
+    lastTime = currentTime;
+
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
+
+    //cout << xpos << "\t" << ypos <<  endl;
+    float xmov = xpos/(X_WIN_SIZE/2) - 1.0;
+    float ymov = -(ypos/(Y_WIN_SIZE/2) - 1.0);
+
+    float MouseSpeed = 10.0f;
+    float moveSpeed = 0.3;
+    xmov *= MouseSpeed * time_delta;
+    ymov *= MouseSpeed * time_delta;
+
+    camera_pos.update_dir(xmov,ymov);
+
+    // Move forward
+    if (glfwGetKey( window, GLFW_KEY_UP ) == GLFW_PRESS){
+        camera_pos.move_pos_in_dir(moveSpeed);
+    }
+    // Move backward
+    if (glfwGetKey( window, GLFW_KEY_DOWN ) == GLFW_PRESS){
+        camera_pos.move_pos_in_dir(-moveSpeed);
+    }
+    // Strafe right
+    if (glfwGetKey( window, GLFW_KEY_RIGHT ) == GLFW_PRESS){
+        camera_pos.move_right(moveSpeed);
+    }
+    // Strafe left
+    if (glfwGetKey( window, GLFW_KEY_LEFT ) == GLFW_PRESS){
+        camera_pos.move_right(-moveSpeed);
+    }
+
+    // Reset mouse position for next frame
+    glfwSetCursorPos(window, X_WIN_SIZE/2, Y_WIN_SIZE/2);
+}
 
 int main( void )
 {
     setup_window();
     //glDepthFunc(GL_LESS);
     // Dark blue background
+
+    glfwPollEvents();
+    glfwSetCursorPos(window, X_WIN_SIZE/2, Y_WIN_SIZE/2);
+
     glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
 
     // Create and compile our GLSL program from the shaders
     GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
@@ -104,16 +213,19 @@ int main( void )
 
 	// Projection matrix : 45� Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
 	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
-	// Camera matrix
+    // Camera matrix
+    glm::mat4 rotate = get_roated_mat(glm::vec3(100,100,100),0.5,3.2);
+    cout << glm::to_string(rotate) << endl;
+    //exit(0);
 	glm::mat4 View       = glm::lookAt(
 								glm::vec3(50,20,-30), // Camera is at (4,3,-3), in World Space
-								glm::vec3(0,0,0), // and looks at the origin
-								glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+                                glm::vec3(50,21,-30), // and looks at the origin
+                                glm::normalize(glm::vec3(0.2123,0.531,0))  // Head is up (set to 0,-1,0 to look upside-down)
 						   );
 	// Model matrix : an identity matrix (model will be at the origin)
 	glm::mat4 Model      = glm::mat4(1.0f);
 	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+    CameraPosition camera_pos(glm::vec3(50,20,-30),glm::vec3(-1,0,0));
 
 
     CubeData all_cubes;
@@ -129,6 +241,8 @@ int main( void )
     do{
         //sleeps when frame was recently rendered to prevent spinning
         count.render_pause();
+
+        move_cursor(camera_pos);
 
         all_cubes.update();
         vector<BYTE> cube_colors;
@@ -157,6 +271,9 @@ int main( void )
 
         // Use our shader
         glUseProgram(programID);
+
+        glm::mat4 MVP      = Projection * camera_pos.veiw_mat() * Model; // Remember, matrix multiplication is the other way around
+        //cout << glm::to_string(MVP) << endl;
 
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
         // 1rst attribute buffer : vertices
@@ -319,7 +436,7 @@ void setup_window(){
 
 
     // Open a window and create its OpenGL context
-    window = glfwCreateWindow( 1024, 768, "Tutorial 02 - Red triangle", NULL, NULL);
+    window = glfwCreateWindow( X_WIN_SIZE, Y_WIN_SIZE, "Tutorial 02 - Red triangle", NULL, NULL);
     if( window == NULL ){
         fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
         getchar();
