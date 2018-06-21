cce2d1fa8f25859ab199860122006e58055d12b3
ben black <weepingwillowben@gmail.com>
2018-06-21 08:43:38 -0700

    added parameters

---
+++ parameters.h
@@ -0,0 +1,11 @@
+#pragma once
+// important mechanical constants
+constexpr float seconds_per_calc = 0.001;
+constexpr float attraction_force_coef = 10000.0;
+constexpr float liquid_pressure_coef = 0.8;
+constexpr float solid_pressure_coef = 0.2;
+constexpr float gass_pressure_coef = 10.0;
+constexpr float gravity_constant = 1000;
+
+
+//important graphical constants
